5da75f2b3259ecbbf2023fddedc7305ad1a1e0bf
aaldifauzan <aldifauzan2047@gmail.com>
2024-03-08 01:30:32 +0700

    Kecamatan-Kelurahan deleted-3

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -27,17 +27,24 @@
 
             <div class="form-group">
                 <label for="exampleFormControlSelect1">Provinsi</label>
-                <select class="form-control" id="provinsi" name="provinsi" required>
-                    <option value="">-- Provinsi --</option>
+                <select class="form-control @error('provinsi') is-invalid @enderror" id="provinsi" name="provinsi" required>
+                    <option value="" @if(old('provinsi') == '') selected @endif>-- Provinsi --</option>
                     @foreach ($provinces as $provinsi)
-                        <option value="{{ $provinsi->id }}">{{ $provinsi->name }}</option>
+                        <option value="{{ $provinsi->id }}" @if(old('provinsi') == $provinsi->id) selected @endif>
+                            {{ $provinsi->name }}
+                        </option>
                     @endforeach
                 </select>
+                @error('provinsi')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlSelect2">Kabupaten/Kota</label>
-                <select class="form-control" id="kabupaten">
+                <select class="form-control @error('kabupaten') is-invalid @enderror" id="kabupaten" name="kabupaten">
 
                 </select>
             </div>
