57e3461c9c9911c464bf07712eabd71bb078831a
aaldifauzan <aldifauzan2047@gmail.com>
2024-03-09 00:39:27 +0700

    Maatwebsite/excel installed

--- config/app.php
+++ config/app.php
@@ -171,6 +171,7 @@ return [
 
         // Provider Lain
         AzisHapidin\IndoRegion\IndoRegionServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -185,6 +186,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
